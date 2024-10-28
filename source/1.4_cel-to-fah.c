bf1ec9eca7b5066d2779a2d2757069d5494180dd
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-10-28 19:20:42 +0900

    add .clangd-format update 1.1 1.2 1.4

--- source/1.4_cel-to-fah.c
+++ source/1.4_cel-to-fah.c
@@ -5,11 +5,12 @@
 #define STEP 20   /* ステップ・サイズ */
 
 /* 摂氏−華氏の対応表を印字する */
-int main() {
-  int fahr;
+int main()
+{
+    int fahr;
 
-  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
-  return 0;
+    return 0;
 }
